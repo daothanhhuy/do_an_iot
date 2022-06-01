ce5f1ea454530c84219bb66509363a96c52153a1
daothanhhuy <19521601@gm.uit.edu.vn>
2022-06-01 15:18:22 +0700

    Them refresh device qua _id

---
+++ src/app/controllers/RefreshController.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Device = require('../../models/devices.js')
+class RefreshController{
+    refreshDevice(req, res){
+        var  _id = req.body._id // gửi với dạng String
+    
+        // Tạo filter tìm kiếm dựa trên các trường
+        const filter = {_id: _id}
+        console.log(`The _id you are query is: ${_id}`)
+    
+    
+        // Code này sẽ chạy bất đồng bộ
+        // {new: true}, trả về document SAU khi đã update dữ liệu,
+        // {new: false}, trả về document TRC khi đã update dữ liệu, nghĩa là t thấy bản cũ của document
+        Device.findOneAndUpdate(filter, {updatedAt: () => Date.now()},{new: true})
+        .then(result => {
+            console.log(result) // hiện thị document
+            res.status(200).send(result)
+        })
+        .catch(err => {
+            console.log('Có lỗi khi query: ')
+            console.log(err)
+            res.status(404).send(`Can't find device _id`)
+        })
+    }
+}
+
+module.exports = new RefreshController()
---
+++ src/routes/refresh.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const RefreshController = require('../app/controllers/RefreshController.js')
+
+router.patch('/', RefreshController.refreshDevice)
+module.exports = router

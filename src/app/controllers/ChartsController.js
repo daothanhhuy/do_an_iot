884a1f8f577436e7899dec4e8358dff2656e3839
Trương Huỳnh Quý <80085196+truonghuynhquy@users.noreply.github.com>
2022-06-02 14:18:49 +0700

    charts/charts_model

---
+++ src/app/controllers/ChartsController.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const DHT = require('../../models/dht.js');
+const BH = require('../../models/bh.js');
+const SL = require('../../models/soil.js');
+//var { Chart } = require('charts.js');
+//import 'chartjs-adapter-luxon';
+//var ChartStreaming = require('chartjs-plugin-streaming');
+
+//Chart.register(ChartStreaming);
+let host = ""
+class ChartsController {
+    // GET /
+    async index(req, res, next) {
+        var title = 'Charts';
+        const dht = await DHT.findOne().sort({ updatedAt: -1 });
+        const bh = await BH.findOne().sort({ updatedAt: -1 });
+        const sl = await SL.findOne().sort({updatedAt: -1});
+        let data = {
+            temp: dht.temp,
+            humi: dht.humi,
+            lux: bh.lux,
+            soildHumi: sl.soildHumi,
+        };
+        res.render('charts/show', { data: data, title: title, host: host });
+    }
+    async getdata(req, res, next) {
+        const dht = await DHT.findOne().sort({ createdAt: -1 });
+        const bh = await BH.findOne().sort({ createdAt: -1 });
+        const sl = await SIL.findOne().sort({ createdAt: -1 });
+        let data = {
+            temp: dht.temp,
+            humi: dht.humi,
+            lux: bh.lux,
+            soildHumi: sl.soildHumi,
+        };
+        res.json({ data: data });
+    }
+}
+
+module.exports = new ChartsController();

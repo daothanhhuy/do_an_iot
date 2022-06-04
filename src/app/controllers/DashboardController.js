342ec6e681bf3b1b60ee9655850bfecb46ac490a
Ceasar Tran <tranquocc1@gmail.com>
2022-06-04 13:09:57 +0700

    Added dashboard layout

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose');
 const User = require('../../models/user.js');
+const Device = require('../../models/devices.js');
 var session;
 class DashboardController {
-    index(req, res, next) {
+    async index(req, res, next) {
         session = req.session;
         if (session.userid) {
-            var title = "Dashboard";
-            res.render('dashboard/show', {
-                title: title,
+            const title = 'Dashboard';
+        // Lấy dữ liệu từ database
+        const devicesInfo = await Device.find();
+        const devices = [];
+        devicesInfo.forEach((result) => {
+            let isAlive = true;
+            let timeLeft =
+                (new Date().getTime() - result.updatedAt.getTime()) / 1000;
+            if (timeLeft > 300) {
+                isAlive = false;
+            }
+            let updatedAt = result.updatedAt.toLocaleDateString();
+            devices.push({
+                name: result.name,
+                imgUrl: result.imgUrl,
+                updatedAt: updatedAt,
+                isAlive: isAlive,
             });
+        });
+        // var tittle = 'Dashboard';
+        res.render('dashboard/show', { 
+            devices: devices, 
+            title: title 
+        });
         } else res.redirect('/');
     }
 }
